ac9801de0dfdf65a5b7c217da0b895ef1b8bc6b5
mennarihan5 <menna1986@live.com>
2023-11-17 12:58:22 +0200

    first angular components

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CourseComponent } from './course/course.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    SidebarComponent,
+    CourseComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
